bce09dee489db75af7cd078ca6de0523411f3fc1
hhhallan <allanstm76@gmail.com>
2025-08-26 11:37:08 +0200

    update responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,7 +44,7 @@ export default function Home() {
         </CardContent>
       </Card>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
         {stats.map((stat, i) => (
           <KpiCard item={stat} key={i} />
         ))}
